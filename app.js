053b15b7ca36b96c45f2d666ff80dfe01ad30f06
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2021-02-06 21:47:59 +0100

    internacjonalizacja

--- app.js
+++ app.js
@@ -40,8 +40,28 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
+
+
+const i18n = require('i18n');
+i18n.configure({
+  locales: ['pl', 'en'],
+  directory: path.join(__dirname, 'locales'),
+  objectNotation: true,
+  cookie: 'acme-hr-lang',
+  defaultLocale: 'pl',
+  register: global
+});
+
+app.use((req, res, next) => {
+  if (!res.locals.lang) {
+    const currentLang = req.cookies['acme-hr-lang'];
+    res.locals.lang = currentLang;
+  }
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/students', authUtils.permitAuthenticatedUser, studentRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ const AuthController = require('../controllers/authController');
 router.post('/login', AuthController.login);
 router.get('/logout', AuthController.logout);
 
+const LangController = require('../controllers/LangController');
+router.get('/changeLang/:lang', LangController.changeLang);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { navLocation: 'main' });
 });
+
+
 
 module.exports = router;
